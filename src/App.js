0532d8e6892ed211d38387a81cf3cb9fc2c6486c
Topsymac <temitopsy35@gmail.com>
2023-02-06 13:24:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import './App.css';
 
 function App() {
+  const [users, setUser] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+
+    const fetchData = async() => {
+      try {
+        const fetchedUsers = await axios.get("https://jsonplaceholder.typicode.com/users")
+        setUser(fetchedUsers.data)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+      }
+    }
+
+    fetchData();
+  }, [])
+
+  console.log(users)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading? (
+        <div className="loader">Loading...</div>
+      ) : (
+        <div className="users">
+          {users.map((user) => (
+            <div className="user-card" key={user.id}>{user.name}</div>
+          ))}
+        </div>
+      )}
+      
     </div>
   );
 }
